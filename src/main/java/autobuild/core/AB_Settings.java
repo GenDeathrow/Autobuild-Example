48989727f0f6ebcf0c0e8f7881babf23cc724367
Funwayguy <FunwayguyEmail@gmail.com>
2014-08-28 11:52:49 +1000

    Setup build number key

--- src/main/java/autobuild/core/AB_Settings.java
+++ src/main/java/autobuild/core/AB_Settings.java
@@ -4,6 +4,6 @@ public class AB_Settings
 {
 	public static final String name = "AutoBuild";
 	public static final String modID = "autobuild";
-	public static final String version = "1.0.0";
+	public static final String version = "AUTO_BUILD_KEY";
 	public static final String proxy = "autobuild.core.proxies";
 }
